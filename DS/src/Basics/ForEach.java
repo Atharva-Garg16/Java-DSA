8bde46428b7145e6119f0d0dbe376146f000ba2b
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-05-22 06:54:26 +0530

    for each loop

--- DS/src/Basics/ForEach.java
+++ DS/src/Basics/ForEach.java
@@ -4,7 +4,7 @@ public class ForEach {
     public static void main(String[] args) {
        int[] arr={2,3,4,5,6,7,8};
        for (int i:arr){
-           System.out.print(STR."\{i} , ");
+           System.out.print(i+" ");
        }
         System.out.print("\b\b");
     }

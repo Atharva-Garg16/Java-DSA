9d16f091336970c78ac68eba515fcd781550444f
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-05-22 06:53:49 +0530

    for each loop

---
+++ DS/src/Basics/ForEach.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class ForEach {
+    public static void main(String[] args) {
+       int[] arr={2,3,4,5,6,7,8};
+       for (int i:arr){
+           System.out.print(STR."\{i} , ");
+       }
+        System.out.print("\b\b");
+    }
+}
